d49b2e944d7bcb4d58ea36fb310614daaec87d37
Valery Tambe <valery.tambe@asda.co.uk>
2024-01-10 12:08:08 +0000

    Moved the creation of items to Trello

---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+
+def get_items():
+    pass
+
+def add_item(title):
+    reqUrl = "https://trello-proxy.azure-api.net/1/cards"
+
+    query_params = {
+        "key": os.getenv("TRELLO_API_KEY"),
+        "token": os.getenv("TRELLO_API_TOKEN"),
+        "idList": os.getenv("TRELLO_TODO_LIST_ID"),
+        "name": title
+        
+    }
+
+    response = requests.post(reqUrl, params= query_params)
+
+    print(response.text)
