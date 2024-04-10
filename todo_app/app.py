9af886f16c0a9e96a5a84625420f96c7dc65211d
TambeVT Tambe <valery.tambe@asda.co.uk>
2024-04-10 14:14:26 +0000

    Add create app function

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, render_template, request
 from todo_app.data.trello_items import add_item, get_items, move_item_to_done
+from todo_app.data.view_model import viewmodel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
 @app.route('/')
 def index():
@@ -16,11 +18,14 @@ def add_todo():
     new_todo_title = request.form.get('title')
     add_item(new_todo_title)  
     return redirect('/')
-    
+
+
 @app.route('/complete-item/<todo_id>' , methods=["POST"])
 def complete_item(todo_id):
     move_item_to_done(todo_id)
     return redirect("/")   
+ 
+    return app
 
 
 
