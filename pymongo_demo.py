edc02a5a89334fed952fcdb84ab402eb69d77789
TambeVT Tambe <valery.tambe@asda.co.uk>
2024-08-07 11:01:15 +0000

    Complete the pymongo demo file

--- pymongo_demo.py
+++ pymongo_demo.py
@@ -13,3 +13,21 @@ collection = db.todo_items
 print(collection)
 
 print(client.list_database_names())
+
+
+# Insert a document into the database
+collection.insert_one({"name": "A pymongo todo", "status": "To Do"})
+
+# Read the documents from the DB into a Python list
+documents = list(collection.find())
+
+document = documents[0]
+
+# Update a document
+collection.update_one({"_id": document["_id"]}, {"$set": {"status": "Done"}})
+
+
+# Read the documents from the DB into a Python list
+documents = list(collection.find())
+
+print(documents)
